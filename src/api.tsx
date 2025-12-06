44208efdc7c8c8f56471a30d01e4a338aca856a5
Kombiah <kombiahinhc@gmail.com>
2025-12-06 18:34:22 +0530

    updated

--- src/api.tsx
+++ src/api.tsx
@@ -25,7 +25,7 @@ export const useApi = () => {
         if (currentUser) {
           config.headers = config.headers || {};
           config.headers["Audit_user_id"] = currentUser._id;
-          if (currentUser.token) {
+          if (currentUser?.token) {
             config.headers["Authorization"] = `Bearer ${currentUser.token}`;
           }
         }
