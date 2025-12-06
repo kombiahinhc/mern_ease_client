ff277eff71c2ec53f785487296c2ea68e13b89f2
Kombiah <kombiahinhc@gmail.com>
2025-12-06 17:41:56 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://mern-admin-chakra-ui.vercel.app',
         secure: true,
       },
     },
